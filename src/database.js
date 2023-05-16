7c05ad4b86257952e5aadc563ef1b063eb84e35b
Santiago Monteiro <santiago.a.monteiro@gmail.com>
2023-05-16 10:47:52 -0400

    initial commit

---
+++ src/database.js
@@ -0,0 +1,69 @@
+import fs from 'node:fs/promises'
+
+const databasePath = new URL('db.json', import.meta.url)
+
+export class Database {
+  #database = {}
+
+  constructor() {
+    fs.readFile(databasePath, 'utf8').then((data) => {
+      this.#database = JSON.parse(data)
+    }).catch(() => {
+      this.#persist()
+    })
+  }
+
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database))
+  }
+
+  select(table, search) {
+    const data = this.#database[table] ?? []
+
+    if (search) {
+      const filteredData = data.filter((row) => {
+        return Object.entries(search).some(([key, value]) => {
+          return row[key].toLowerCase().includes(value.toLowerCase())
+        })
+      })
+
+      return filteredData
+    }
+
+    return data
+  }
+
+  insert(table, data) {
+    if (Array.isArray(this.#database[table])) {
+      this.#database[table].push(data)
+    } else {
+      this.#database[table] = [data]
+    }
+
+    this.#persist()
+
+    return data
+  }
+
+  update(table, id, data) {
+    const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+    const theItemToBeDeletedExists = rowIndex > -1
+
+    if (theItemToBeDeletedExists) {
+      this.#database[table][rowIndex] = { id, ...data }
+      this.#persist()
+    }
+  }
+
+  delete(table, id) {
+    const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+    const theItemToBeDeletedExists = rowIndex > -1
+
+    if (theItemToBeDeletedExists) {
+      this.#database[table].splice(rowIndex, 1)
+      this.#persist()  
+    }
+  }
+}
